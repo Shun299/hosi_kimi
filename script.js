0832220404794612cc72103e2a9d8e2b1fda10aa
Shun299 <23610088ss@stu.yamato-u.ac.jp>
2026-01-31 17:05:19 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('active');
+                observer.unobserve(entry.target);
+            }
+        })
+    },options);
+
+    const partmoveElements = document.querySelectorAll('.partmove, .catchmove');
+    partmoveElements.forEach(el => observer.observe(el));
+});
